126368e64d63d992f1b97cafd8b03a2f90a1532e
lee <logicleeee+github@gmail.com>
2018-10-31 11:06:06 -0700

    initial commit

---
+++ bubble-sort.js
@@ -0,0 +1,27 @@
+// bubble sort 
+/* 
+    for each pass, push the highest number to the end of the list (highestIndex) 
+    by comparing and sorting adjacent pairs until there are no swaps.
+*/
+
+function bubbleSort(arr){
+    let sorted = false
+    let highestIndex = arr.length
+    const swapInArray = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]]
+
+    while (sorted === false) {
+        sorted = true
+
+        for (let i=0; i < highestIndex; i++) {
+            if (arr[i] > arr[i+1]) {
+                sorted = false
+                swapInArray(arr,i,i+1)
+            }
+        }
+        highestIndex -= 1
+    }
+    return arr
+}
+
+// TODO write better tests for bubble sort 
+console.log('BubbleSort   ', bubbleSort([9,8,7,6,5,4,3,2,1,0,-1,-2]))
---
+++ selection-sort.js
@@ -0,0 +1,23 @@
+// selection sort 
+// for each pass, find the lowest value and put it at index 0 of the pass
+function selectionSort(arr){
+    let lowestIndex
+    const swapInArray = (arr, i, j) => [arr[i], arr[j]] = [arr[j], arr[i]]
+
+    for (let i = 0; i < arr.length; i++) {
+        lowestIndex = i
+
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j] < arr[lowestIndex]) {
+                lowestIndex = j
+            }
+        }
+
+        if (i != lowestIndex) {
+            swapInArray(arr,i,lowestIndex)
+        }
+    }
+    return arr
+}
+
+console.log('selectionSort', selectionSort([9,8,7,6,5,4,3,2,1,0,-1,-2]))
